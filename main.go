7e9962b216aeeccec30877aa1fa44b8f45cbd944
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-09-25 08:01:00 +0500

    update main.go

--- main.go
+++ main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"fmt"
+	"github.com/google/uuid"
+	"math/rand"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jaswdr/faker/v2"
 )
+
+type User struct {
+	Id    string `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Age   int    `json:"age"`
+}
 
 func main() {
 	server := gin.Default()
@@ -15,9 +25,31 @@ func main() {
 			"message": "pong",
 		})
 	})
+	server.GET("/users", func(c *gin.Context) {
+		users := generateMockUsers(10)
+		c.JSON(http.StatusOK, users)
+	})
 
 	err := server.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 	if err != nil {
 		fmt.Println("Error: ", err)
 	}
+}
+
+func generateMockUsers(n int) []User {
+	fake := faker.New()
+	var users []User
+
+	for i := 0; i < n; i++ {
+		user := User{
+			Id:    uuid.New().String(),
+			Name:  fake.Person().Name(),
+			Email: fake.Internet().Email(),
+			Age:   rand.Intn(100),
+		}
+
+		users = append(users, user)
+	}
+
+	return users
 }
