bd2be548817ce9eecaa234ccc167701bf042cefd
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-09-25 07:40:25 +0500

    init gin server

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	err := server.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+}
