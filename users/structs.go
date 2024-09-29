d9d5e6c7009eb1fd25faf8adb16102bbd4ac4a6e
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-09-29 06:41:46 +0500

    add User.Role

--- users/structs.go
+++
@@ -1,8 +0,0 @@
-package users
-
-type User struct {
-	Id    string `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
-	Age   int    `json:"age"`
-}
---
+++ users/user.entity.go
@@ -0,0 +1,9 @@
+package users
+
+type User struct {
+	Id    string   `json:"id"`
+	Name  string   `json:"name"`
+	Email string   `json:"email"`
+	Age   int      `json:"age"`
+	Role  UserRole `json:"role"`
+}
