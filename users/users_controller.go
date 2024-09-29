afee3a7c916437c8e5eec0fef39810d8b2a2a5bc
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-09-29 08:34:43 +0500

    update UsersController

--- users/users_controller.go
+++ users/users_controller.go
@@ -7,7 +7,7 @@ import (
 )
 
 type UsersController struct {
-	usersService *UsersService
+	usersService IUsersService
 }
 
 func NewUsersController() *UsersController {
@@ -17,41 +17,129 @@ func NewUsersController() *UsersController {
 }
 
 func (c *UsersController) Init(engine *gin.Engine) {
-	engine.GET("/users", func(ctx *gin.Context) {
-		limit, err := strconv.Atoi(ctx.DefaultQuery("limit", "10"))
-		if err != nil {
-			ctx.JSON(400, core.BadRequestResponse{core.Response{
-				Error: &core.ResponseError{Message: "Invalid limit"},
-			}})
-			return
-		}
+	engine.GET("/users", c.find)
+	engine.GET("/users/:id", c.findById)
+	engine.POST("/users", c.create)
+	engine.PUT("/users", c.update)
+	engine.PATCH("/users/:id", c.partialUpdate)
+	engine.DELETE("/users/:id", c.delete)
+}
 
-		offset, err := strconv.Atoi(ctx.DefaultQuery("offset", "0"))
-		if err != nil {
-			ctx.JSON(400, core.BadRequestResponse{core.Response{
-				Error: &core.ResponseError{Message: "Invalid offset"},
-			}})
-			return
-		}
+func (c *UsersController) find(ctx *gin.Context) {
+	limit, err := strconv.Atoi(ctx.DefaultQuery("limit", "10"))
+	if err != nil {
+		ctx.JSON(400, core.NewBadRequestResponse("Invalid limit"))
+		return
+	}
 
-		params := UsersServiceFindParams{
-			Limit:  limit,
-			Offset: offset,
-		}
+	offset, err := strconv.Atoi(ctx.DefaultQuery("offset", "0"))
+	if err != nil {
+		ctx.JSON(400, core.NewBadRequestResponse("Invalid offset"))
+		return
+	}
 
-		users, err := c.usersService.find(params)
-		if err != nil {
-			ctx.JSON(500, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-		ctx.JSON(200, core.Response{Data: users})
-	})
+	params := UsersServiceFindParams{
+		Limit:  limit,
+		Offset: offset,
+	}
 
-	//engine.GET("/users/:id", c.findById)
-	//engine.POST("/users", c.create)
-	//engine.PUT("/users", c.update)
-	//engine.PATCH("/users/:id", c.partialUpdate)
-	//engine.DELETE("/users/:id", c.delete)
+	users, err := c.usersService.find(params)
+	if err != nil {
+		ctx.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	ctx.JSON(200, core.Response[[]User]{Data: &users})
+}
+
+func (c *UsersController) findById(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	user, err := c.usersService.findById(id)
+	if err != nil {
+		ctx.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	if user == nil {
+		ctx.JSON(404, core.NewBadRequestResponse("User not found"))
+		return
+	}
+	ctx.JSON(200, core.Response[User]{Data: user})
+}
+
+func (c *UsersController) create(ctx *gin.Context) {
+	var userCreate UserCreate
+	err := ctx.ShouldBindJSON(&userCreate)
+	if err != nil {
+		ctx.JSON(400, core.NewBadRequestResponse(err.Error()))
+		return
+	}
+
+	newUser, err := c.usersService.create(userCreate)
+	if err != nil {
+		ctx.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(201, core.Response[User]{Data: &newUser})
+}
+
+func (c *UsersController) update(ctx *gin.Context) {
+	var user UserUpdate
+	err := ctx.ShouldBindJSON(&user)
+	if err != nil {
+		ctx.JSON(400, core.NewBadRequestResponse(err.Error()))
+		return
+	}
+
+	updatedUser, err := c.usersService.update(user)
+	if err != nil {
+		ctx.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(200, core.Response[User]{Data: &updatedUser})
+}
+
+func (c *UsersController) partialUpdate(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	var user UserPartialUpdate
+	err := ctx.ShouldBindJSON(&user)
+	if err != nil {
+		ctx.JSON(400, core.NewBadRequestResponse(err.Error()))
+		return
+	}
+
+	updatedUser, err := c.usersService.partialUpdate(id, user)
+	if err != nil {
+		ctx.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(200, core.Response[User]{Data: &updatedUser})
+}
+
+func (c *UsersController) delete(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	err := c.usersService.delete(id)
+	if err != nil {
+		ctx.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(204, nil)
 }
