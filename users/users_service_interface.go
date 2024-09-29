6fc35ad2698accfeb4c9f20cf084309070005881
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2024-09-29 07:18:42 +0500

    update IUsersService

--- users/users_service_interface.go
+++ users/users_service_interface.go
@@ -1,11 +1,11 @@
 package users
 
 type IUsersService interface {
-	find(params UsersServiceFindParams) (*FindUsersResponse, error)
-	findById(id string) (*User, error)
-	create(user UserCreate) (*User, error)
-	update(user UserUpdate) (*User, error)
-	partialUpdate(id string, user UserPartialUpdate) (*User, error)
+	find(params UsersServiceFindParams) ([]User, error)
+	findById(id string) (User, error)
+	create(user UserCreate) (User, error)
+	update(user UserUpdate) (User, error)
+	partialUpdate(id string, user UserPartialUpdate) (User, error)
 	delete(id string) error
 }
 
